e5f45600f7e3f4e4f17d490f7eff1c3c2da8cfe0
Ansh Goyal <anshg1214@gmail.com>
2021-12-01 11:22:25 +0530

    build: add vercel.json

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.listen(process.env.PORT || 3000, () => {
 
 
 app.use(express.json());
-app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
